9c5afdfe2125b504c910ad3c91f4950b51a1cbaa
aldoprogrammer <aldobesma@gmail.com>
2024-03-03 10:18:34 +0700

    update

--- index.js
+++ index.js
@@ -11,12 +11,7 @@ const app = express();
 const FRONTEND_URL = process.env.FRONTEND_URL;
 console.log("FRONTEND_URL:", FRONTEND_URL);
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", FRONTEND_URL);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
